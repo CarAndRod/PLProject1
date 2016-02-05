1a1d1f3e982e0cbf9d8ce2fb03845a3e69927076
Carlos A Rodriguez <c.rodz29@gmail.com>
2016-02-04 22:41:53 -0600

    DB testing with Barbara

--- src/schlomer/CustomerDB.java
+++ src/schlomer/CustomerDB.java
@@ -3,5 +3,20 @@ package schlomer;
 /**
  * Created by daniel on 2/4/16.
  */
-public class CustomerDB {
+public class CustomerDB
+{
+    public Customer customerFinder (int customerNumber)
+    {
+        if (customerNumber == 1001)
+        {
+            Customer Barbara = new Customer();
+            Barbara.setName("Barbara White");
+            Barbara.setAddress("3400 Richmond Parkway #3423");
+            Barbara.setCity("Bristol");
+            Barbara.setState("CT");
+            Barbara.setPostalCode(06010);
+            return Barbara;
+        }
+        return null;
+    }
 }
