ade8484c737b6d0685b578f0cda3be83fd978f49
Daniel Schlomer <djschlomer@gmail.com>
2016-02-04 22:28:00 -0600

    Customer class ready for testing

--- src/schlomer/Customer.java
+++ src/schlomer/Customer.java
@@ -2,9 +2,55 @@ package schlomer;
 
 public class Customer {
 
-    public static void main(String[] args) {
-        // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, World");
+    private String name;
+    private String address;
+    private String city;
+    private String state;
+    private int postalCode;
+
+    public void setName(String CustomerName) {
+        this.name = CustomerName;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setAddress(String CustomerAddress) {
+        this.address = CustomerAddress;
+    }
+
+    public String getAddress() {
+        return this.address;
+    }
+
+    public void setCity(String CustomerCity) {
+        this.city = CustomerCity;
+    }
+
+    public String getCity() {
+        return this.city;
+    }
+
+    public void setState(String CustomerState) {
+        this.state = CustomerState;
+    }
+
+    public String getState() {
+        return this.state;
+    }
+
+    public void setPostalCode(int CustomerPostalCode) {
+        this.postalCode = CustomerPostalCode;
+    }
+
+    public int getPostalCode() {
+        return this.postalCode;
+    }
+
+    public String getNameAndAddress() {
+        return this.name + "\n" + this.address + "\n" +
+                this.city + ", " + this.state + " " + this.postalCode;
     }
 
 }
