92722bcef40b6135a114cef87e056602e3fb14d0
Daniel Schlomer <djschlomer@gmail.com>
2016-02-04 21:24:07 -0600

    Customer class

---
+++ src/schlomer/Customer.java
@@ -0,0 +1,10 @@
+package schlomer;
+
+public class Customer {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+
+}
